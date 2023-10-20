66c55ba2b79d45f546d503273bc54b14cb1495bd
Radhika Gupte <gupter@oregonstate.edu>
2023-10-20 00:12:00 -0700

    added implementation for url shortening

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-const uri = process.env.MONGO_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,
-});
-
 app.use(cors());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-
-app.use(cors());
 app.use(express.json());
-app.use('/public', express.static(process.cwd() + '/public'));
+
+const createNewUrl = require('./myApp.js').createNewUrl;
+app.post('/api/shorturl', async (req, res) => {
+  const url = req.body.url;
+  console.log(url);
+  let newUrl = createNewUrl(url);
+
+  newUrl.then(function (newUrl) {
+    console.log(newUrl);
+    res.send(newUrl);
+  });
+});
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
--- myApp.js
+++ myApp.js
@@ -1,11 +1,27 @@
 require('dotenv').config();
+let express = require('express');
+let app = express();
 let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+const shortid = require('shortid');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 let URL;
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'Connection error: '));
+connection.once('open', () => {
+  console.log('Successfully establishd connction with MongoDB database');
 });
 
 const urlSchema = new mongoose.Schema({
@@ -18,3 +34,42 @@ const urlSchema = new mongoose.Schema({
   },
 });
 URL = mongoose.model('URL', urlSchema);
+
+const urlregex =
+  /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
+const createNewUrl = async (url) => {
+  const urlCode = shortid.generate(url);
+
+  if (!urlregex.test(url)) {
+    return { error: 'invalid url' };
+  } else {
+    try {
+      let existing = await URL.findOne({
+        original_url: url,
+      });
+
+      if (existing) {
+        return {
+          original_url: existing.original_url,
+          short_url: existing.short_url,
+        };
+      } else {
+        const new_url = new URL({
+          original_url: url,
+          short_url: urlCode,
+        });
+        await new_url.save();
+        return {
+          original_url: new_url.original_url,
+          short_url: new_url.short_url,
+        };
+      }
+    } catch (err) {
+      return { error: 'Bad Request' };
+    }
+  }
+};
+
+exports.URLModel = URL;
+exports.createNewUrl = createNewUrl;
